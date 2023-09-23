27b6f33d45e6082291017951c1819d8d02b5f9e4
Rurix <agashkovi@mail.ru>
2023-09-23 17:42:10 +0300

    Update lession Torch

--- pyLearn.py
+++ pyLearn.py
@@ -1,35 +1,34 @@
 # import libaries
 import torch as tch
+import torch.nn as nn
 
 # Initialisation (Model:'y = 2*x')
 X = tch.tensor([1, 2, 3, 4], dtype = tch.float32)
 Y = tch.tensor([2, 4, 6, 8], dtype = tch.float32)
 omega = tch.tensor(0.0, dtype=tch.float32, requires_grad=True)
 
-lr = 0.01   # Learning  rate  
-n = 1000      # Number of iterations
+lr = 0.01       # Learning  rate  
+n = 1000        # Number of iterations
 
 # Model prediction (H)
 def forward(omega, X):
     return omega * X
 
-# Loss - Mean square error (MSE)
-def loss(y, H):
-    return ((y-H)**2).mean()
-
 # Training
+optimizer = tch.optim.SGD([omega], lr=lr)
+loss = nn.MSELoss()
 for epoch in range(n):
     # Prediction
     H = forward(omega, X)
     # Loss
-    L = loss(Y, H)
+    L = loss(Y,H)
     # Gradient
     L.backward()
     # Update omega
-    with tch.no_grad():
-        omega -= lr*omega.grad
-    omega.grad.zero_()
-
+    optimizer.step()
+    # Zero geadients
+    optimizer.zero_grad()
+    # Print epoch
     if epoch % 100 == 0:
         print(f'epoch {epoch + 1}: omega = {omega:.3f}, loss = {L:.5f}')
 
